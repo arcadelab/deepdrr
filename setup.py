4c9bf04a78b8179cee9f52aced33afbff83f19b4
Benjamin D. Killeen <benjamindkilleen@gmail.com>
2020-10-12 18:34:38 -0400

    author

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='deepdrr',
     version='0.1.dev0',
-    author='Mathias Unberath, Benjamin D. Killeen',
-    author_email='killeen@jhu.edu',
+    author='Mathias Unberath',
+    author_email='unberath@jhu.edu',
     description='Forked from mathiasunberath/DeepDRR to be more pythonic.',
     long_description=long_description,
     long_description_content_type="text/markdown",
