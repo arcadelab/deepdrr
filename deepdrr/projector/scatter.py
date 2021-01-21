5bb331578bea025849ce98c789fff6d6f0a7ea54
Max Judish <mmjudish@gmail.com>
2021-01-21 14:14:19 -0500

    Begin outlining Monte Carlo scatter simulation

--- deepdrr/projector/scatter.py
+++ deepdrr/projector/scatter.py
@@ -1,71 +1,117 @@
-import logging
-import torch
-import numpy as np
-from skimage.transform import resize
-import torchvision.transforms as transforms
-from pathlib import Path
+#
+# TODO: cite the papers that form the basis of this code
+#
 
-from .network_scatter import SimpleNetGenerator
+from typing import Optional
+
+import logging
+import numpy as np
+from . import spectral_data
+from .. import geo
 
 
 logger = logging.getLogger(__name__)
 
 
-class ScatterNet():
-    def __init__(self):
-        torch.cuda.set_device(0)
-        d = Path(__file__).resolve().parent
-        self.model_path = d / "model_scatter.pth"
-        self.model = SimpleNetGenerator()
-        self.model.load_state_dict(torch.load(self.model_path))
-        self.model = self.model.cuda()
-        self.transform = transforms.Compose([transforms.ToTensor()])
-        logger.info("loaded scatter net.")
+def simulate_scatter_no_vr(
+    volume: np.ndarray,
+    source: geo.Point3D,
+    spectrum: Optional[np.ndarray] = spectral_data.spectrums['90KV_AL40'], 
+    photon_count: Optional[int] = 10000000, # 10^7
+    Eabs: Optional[float] = 1000
+) -> np.ndarray:
+    """Produce a grayscale (intensity-based) image representing the photon scatter during an X-Ray, 
+    without using VR (variance reduction) techniques.
 
-    def add_scatter(self, input_image, camera):
-        scale_factor = 0.1334 * camera.pixel_size[0] / 0.31
-        # input_image = np.ascontiguousarray(np.swapaxes(input_image,0,1))
-        input_image = np.ascontiguousarray(np.swapaxes(input_image, 1, 2))
-        maxlen = np.max(input_image.shape[1:-1]) * scale_factor
-        uselen = np.int(np.power(2, np.ceil(np.log2(maxlen))))
-        max = np.max(input_image)
-        tensor_list = []
-        cuts = []
-        for i in range(0, input_image.shape[0]):
-            image = resize(input_image[i, :, :] / max, [np.int(np.round(input_image.shape[1] * scale_factor)), np.int(np.round(input_image.shape[2] * scale_factor))]) * max * 256 / 5.5
-            cuts = (((uselen - image.shape[0]) // 2, (uselen - image.shape[0]) - (uselen - image.shape[0]) // 2), ((uselen - image.shape[1]) // 2, (uselen - image.shape[1]) - (uselen - image.shape[1]) // 2))
-            image = np.pad(image, cuts, mode='reflect')
-            image.shape = (image.shape[0], image.shape[1], 1)
-            tensor_list.append(self.transform(image).unsqueeze(0))
-        image_tensor = torch.cat(tensor_list).cuda()
-        output = self.model.forward(image_tensor) * 0.10
-        scatter = output.cpu().detach().numpy()
-        scatter = np.squeeze(scatter, 1)
-        # cut relevant part
-        scatter = scatter[:, cuts[0][0]:uselen - cuts[0][1], cuts[1][0]:uselen - cuts[1][1]]
-        out = np.zeros(input_image.shape)
-        for i in range(0, scatter.shape[0]):
-            # edge compensation models decreasing scatter intensity at the borders of the detector (only working for 1240*960 0.31mm detector)
-            # comp_scatter = edge_compensation(scatter[i, :, :])
-            # out[i,:,:]=resize(comp_scatter, input_image[i, :, :].shape, order=3, mode="edge")
-            out[i, :, :] = resize(scatter[i, :, :], input_image[i, :, :].shape, order=3, mode="edge")
-        out = np.swapaxes(out, 1, 2)
-        return out
+    Args:
+        volume (np.ndarray): the volume density data.
+        source (Point3D): the source point for rays in the camera's IJK space
+        spectrum (Optional[np.ndarray], optional): spectrum array.  Defaults to 90KV_AL40 spectrum.
+        photon_count (Optional[int], optional): the number of photons simulated.  Defaults to 10^7 photons.
+        Eabs (Optional[float], optional): the energy (in keV) at or below which photons are assumed to be absorbed by the materials.  Defaults to 1000 (keV).
 
+    Returns:
+        np.ndarray: intensity image of the photon scatter
+    """
+    return NotImplemented
 
-def edge_compensation(img):
-    edge = np.sin(np.linspace(0, np.pi / 2, 45, dtype=np.float32))
-    window_h = np.ones((128), dtype=np.float32)
-    window_h[0:45] = edge
-    window_h[128 - 45:128] = edge[::-1]
-    window_h = window_h * 0.35 + 0.65
-    window_w = np.ones((165), dtype=np.float32)
-    window_w[0:45] = edge
-    window_w[165 - 45:165] = edge[::-1]
-    window_w = window_w * 0.35 + 0.65
-    window = np.outer(window_w, window_h)
-    return img * window
+def track_single_photon_no_vr(
+    initial_pos: geo.Point3D,
+    initial_dir: geo.Vector3D,
+    initial_E: np.float32,
+    N_vals: np.ndarray,
+    sigma_C_vals: np.ndarray,
+    sigma_R_vals: np.ndarray
+) -> np.ndarray:
+    """Produce a grayscale (intensity-based) image representing the photon scatter of a single photon 
+    during an X-Ray, without using VR (variance reduction) techniques.
 
+    Args:
+        initial_pos (geo.Point3D): the initial position of the photon, which is the X-Ray source
+        initial_dir (geo.Vector3D): the initial direction of travel of the photon
+        initital_E (np.float32): the initial energy of the photon
+        N_vals (np.ndarray): a field over the volume of 'N', the number of molecules per unit volume.  Each cell in N_vals contains the information for the corresponding voxel.
+        sigma_C_vals (np.ndarray): a field over the volume of 'sigma_C', the interactional cross-section for Compton scatter.  Each cell in sigma_C_vals contains the information for the corresponding voxel.
+        sigma_R_vals (np.ndarray): a field over the volume of 'sigma_R', the interactional cross-section for Rayleigh scatter.  Each cell in sigma_R_vals contains the information for the corresponding voxel.
 
-if __name__ == "__main__":
-    scatter = ScatterNet()
+    Returns:
+        np.ndarray: intensity image of the photon scatter
+    """
+    return NotImplemented
+
+def get_scattered_dir(
+    dir: geo.Vector3D,
+    theta: np.float32,
+    phi: np.float32
+) -> geo.Vector3D:
+    """Determine the new direction of travel after getting scattered
+
+    Args:
+        dir (geo.Vector3D): the incoming direction of travel
+        theta (np.float32): the polar scattering angle, i.e. the angle dir and dir_prime
+        phi (np.float32): the azimuthal angle, i.e. how dir_prime is rotated about the axis 'dir'.
+
+    Returns:
+        geo.Vector3D: the outgoing direction of travel
+    """
+    return NotImplemented
+
+def sample_U01() -> np.float32:
+    """Returns a value uniformly sampled from the interval [0,1]"""
+    return NotImplemented
+
+def sample_Rayleigh_theta_W(
+    N_val: np.float32, 
+    sigma_val: np.float32 # TODO: are these parameters actually the relevant ones?
+) -> np.float32:
+    """Randomly sample values of theta and W for a given Rayleigh scatter interaction
+
+    Args:
+        N_val: the number of molecules per unit volume at the location of the photon interaction
+        sigma_val: the interactional cross-sectional area at the location of the photon interation
+
+    Returns:
+        np.float32: theta, the polar scattering angle 
+        np.float32: W, the energy loss (will always be zero)
+    """
+    theta = NotImplemented
+    W = 0 # by definition of Rayleigh scatter, E_incident = E_outgoing
+    return theta, W
+
+def sample_Compton_theta_W(
+    N_val: np.float32, 
+    sigma_val: np.float32 # TODO: are these parameters actually the relevant ones?
+) -> np.float32:
+    """Randomly sample values of theta and W for a given Compton scatter interaction
+
+    Args:
+        N_val: the number of molecules per unit volume at the location of the photon interaction
+        sigma_val: the interactional cross-sectional area at the location of the photon interation
+
+    Returns:
+        np.float32: theta, the polar scattering angle 
+        np.float32: W, the energy loss (will always be zero)
+    """
+    theta = NotImplemented
+    W = NotImplemented
+    return theta, W
