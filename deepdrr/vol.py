a7e7843bb170224167e8c38337964ff9e61b35d7
Benjamin D. Killeen <benjamindkilleen@gmail.com>
2021-04-18 07:14:26 -0400

    docs

--- deepdrr/vol.py
+++ deepdrr/vol.py
@@ -531,11 +531,16 @@ class Volume(object):
                 surface.save(cache_path)
         return surface
 
-    def get_mesh_in_world(self, full: bool = False, cache_dir: Optional[Path] = None) -> pv.PolyData:
+    def get_mesh_in_world(self, full: bool = False, cache_dir: Optional[Path] = None, use_cached: bool = True) -> pv.PolyData:
         """Get a pyvista mesh of the outline in world-space.
 
+        Args:
+            full (bool): Whether to render the full volume or just a wireframe. Defaults to False.
+            cache_dir (Optional[Path], optional): a location to cache the bone surface.
+            use_cached (bool): If False, don't use the cached bone surface but re-create it (expensive). Defaults to True.
+
         Returns:
-            pv.PolyData: pyvist mesh.
+            pv.PolyData: pyvista mesh.
         """
 
         assert pv_available, f'PyVista not available for obtaining Volume mesh. Try: `pip install pyvista`'
@@ -567,7 +572,8 @@ class Volume(object):
         mesh += pv.Line(points[3], points[7])
 
         if full:
-            material_mesh = self.get_surface(material='bone', cache_dir=cache_dir)
+            logger.debug(f"getting full surface mesh for volume")
+            material_mesh = self.get_surface(material='bone', cache_dir=cache_dir, use_cached=use_cached)
             material_mesh.transform(geo.get_data(self.world_from_anatomical))
             mesh += material_mesh
 
