83d90d9f5320f9199192516190f6d2fb6543c57c
Benjamin Killeen <benjamindkilleen@gmail.com>
2021-06-16 16:23:01 -0400

    minor changes

--- deepdrr/__init__.py
+++ deepdrr/__init__.py
@@ -2,4 +2,15 @@ from .device import CArm, MobileCArm
 from .vol import Volume
 from .projector import Projector
 
-__all__ = ["MobileCArm", "CArm", "Volume", "Projector"]
+from . import vis, geo, projector, device
+
+__all__ = [
+    "MobileCArm",
+    "CArm",
+    "Volume",
+    "Projector",
+    "vis",
+    "geo",
+    "projector",
+    "device",
+]
