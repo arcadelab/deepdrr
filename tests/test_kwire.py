a447da50408d5a3448ced71aaf7b7aa3d3d9f1a9
Benjamin D. Killeen <benjamindkilleen@gmail.com>
2021-06-23 15:26:04 -0400

    todo kwire test case

---
+++ tests/test_kwire.py
@@ -0,0 +1,2 @@
+# TODO: create a test case possibly using the new dataset, along with some annotations, that tests the KWire alignment code.
+# This will create a test case, demo that the annotations are correct, etc.
