9d0453cbd71305e2ed17e3e81376eda4e9d360e6
Benjamin D. Killeen <benjamindkilleen@gmail.com>
2022-02-03 13:49:12 -0500

    refinements to things

--- deepdrr/utils/image_utils.py
+++ deepdrr/utils/image_utils.py
@@ -1,3 +1,4 @@
+import logging
 import numpy as np
 from PIL import Image
 from pathlib import Path
@@ -8,11 +9,17 @@ def save(path: str, image: np.ndarray) -> None:
 
     Args:
         path (str): the path to write the image to. Also determines the type.
-        image (np.ndarray): the image. If in float32, assumed to be a float image. Converted to uint8 before saving.
+        image (np.ndarray): the image, in [C, H, W] or [H, W, C] order. (If the former, transposes). 
+            If in float32, assumed to be a float image. Converted to uint8 before saving.
     """
+    if len(image.shape) == 3 and image.shape[0] in [3, 4]:
+        image = image.transpose(1, 2, 0)
 
-    if image.dtype in [np.float32, np.float64]:
+    if image.dtype in [np.float16, np.float32, np.float64]:
         image = np.clip(image * 255, 0, 255).astype(np.uint8)
+    elif image.dtype == bool:
+        image = image.astype(np.uint8) * 255
+
     Image.fromarray(image).save(path)
 
 
