a0c2aef366e95c0535ee7a94715f37628b2716b2
wu-qiyuan <84822533+wu-qiyuan@users.noreply.github.com>
2022-04-17 22:44:11 -0400

    Update volume.py

--- deepdrr/vol/volume.py
+++ deepdrr/vol/volume.py
@@ -396,12 +396,12 @@ class Volume(object):
                 # ~/datasets/DeepDRR_Data or the user-specified "root" directory. See
                 # data_utils.download())
                 if use_cached:
-                    segmentation_nnunet = use_nnunet.Segmentation()
+                    segmentation_nnunet = deepdrr.use_nnunet.Segmentation()
                     if cache_dir is None:
                         raise NotImplementedError("TODO")
                     materials = segmentation_nnunet.read_mask(cache_dir,0)  #6:Lung, 17:multi-organ, 0:default
                 else:
-                    segmentation_nnunet = use_nnunet.Segmentation()
+                    segmentation_nnunet = deepdrr.use_nnunet.Segmentation()
                     materials = segmentation_nnunet.nnu_segmentation(path,6)  #6:Lung, 17:multi-organ
                 
 #                 raise NotImplementedError("TODO")
