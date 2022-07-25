8ea54ba8ca30f9ab0c14f23f04762aa073d52ae1
Benjamin Killeen <benjamindkilleen@gmail.com>
2022-07-25 10:06:44 -0400

    changes

--- deepdrr/geo/random.py
+++ deepdrr/geo/random.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import List, Optional, overload
 import numpy as np
 from .core import Vector3D, vector
+
 
 def _sample_spherical(d_phi: float, n: int) -> np.ndarray:
     """Sample n vectors within `phi` radians of [0, 0, 1]."""
@@ -15,9 +16,22 @@ def _sample_spherical(d_phi: float, n: int) -> np.ndarray:
     return np.stack([x, y, z], axis=1)
 
 
-def spherical_uniform(center: Vector3D = [0, 0, 1], d_phi: float = np.pi, n: int = 1) -> List[Vector3D]:
+@overload
+def spherical_unifrom(center: Vector3D, d_phi: float, n: int) -> List[Vector3D]:
+    ...
+
+
+@overload
+def spherical_unifrom(center: Vector3D, d_phi: float, n: None) -> Vector3D:
+    ...
+
+
+def spherical_uniform(center=[0, 0, 1], d_phi=np.pi, n=None):
     """Sample unit vectors within `d_phi` radians of `v`."""
     v = vector(center).hat()
-    points = _sample_spherical(d_phi, n)
+    points = _sample_spherical(d_phi, 1 if n is None else n)
     F = v.rotation(vector(0, 0, 1))
-    return [F @ vector(p) for p in points]
+    if n is None:
+        return F @ vector(points[0])
+    else:
+        return [F @ vector(p) for p in points]
