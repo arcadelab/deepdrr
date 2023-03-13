006d3c4df39181a7862135b26a2be0a0c95a6459
Benjamin Killeen <benjamindkilleen@gmail.com>
2023-03-12 22:33:27 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="deepdrr",
-    version="1.1.1a2",
+    version="1.1.2",
     author="Benjamin D. Killeen",
     author_email="killeen@jhu.edu",
     description="A Catalyst for Machine Learning in Fluoroscopy-guided Procedures.",
