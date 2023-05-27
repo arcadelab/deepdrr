a2d1146328674051b55c59aeeb7020d305586b37
Liam Wang <5054950+liamjwang@users.noreply.github.com>
2023-05-27 11:27:05 -0700

    Add tide algorithm

--- deepdrr/pycuda_ray_surface/pycuda_source.cu
+++ deepdrr/pycuda_ray_surface/pycuda_source.cu
@@ -871,3 +871,96 @@ __global__ void kernelBVHIntersection3(const float* __restrict__ vertices,
         }
     }
 }
+
+
+
+// Function to fix odd ray intersections.
+
+// Method:
+// Start with sorted list of ray intersections
+// Loop through once forwards to generate the altitude before and after each intersection
+// The highest of the two ends is "sea level"
+// Loop through and delete any collision that either starts or ends below sea level
+// To do this, set the intercept Ts to infinity, decrement the count, and set facing to 0
+// Then loop through to fill gaps
+// Keep a pointer at at the highest filled index, and another that sweeps forward palcing into the lowest index
+__device__ void tide(
+    int *interceptCounts,
+    float *interceptTs,
+    int8_t *interceptFacing,
+    int rayIdx
+)
+{
+    int[] altitudes = rayInterceptTs[rayIdx]
+    int altitude = 0;
+    
+    for (int i = 0; i < MAX_INTERSECTIONS; i++) {
+        altitude += interceptFacing[i];
+        altitudes[i] = altitude;
+    }
+
+    int seaLevel = max(0, altitude);
+
+    int prevAltitide = 0;
+    for (int i = 0; i < MAX_INTERSECTIONS; i++) {
+        int currentAltitude = altitudes[i];
+        if (currentAltitude < seaLevel || prevAltitide < seaLevel) {
+            interceptTs[i] = INFINITY;
+            interceptFacing[i] = 0;
+            (*interceptCounts)--;
+        }
+        prevAltitide = currentAltitude;
+    }
+
+    // Fill gaps
+    int dstIdx = 0;
+    int srcIdx = 0;
+
+    while (dstIdx < MAX_INTERSECTIONS && interceptFacing[dstIdx] != 0) {
+        dstIdx++;
+    }
+    srcIdx = dstIdx + 1;
+
+    while (srcIdx < MAX_INTERSECTIONS && dstIdx < MAX_INTERSECTIONS) {
+        while (srcIdx < MAX_INTERSECTIONS && interceptFacing[srcIdx] == 0) {
+            srcIdx++;
+        }
+        if (srcIdx < MAX_INTERSECTIONS) {
+            interceptTs[dstIdx] = interceptTs[srcIdx];
+            interceptFacing[dstIdx] = interceptFacing[srcIdx];
+            interceptTs[srcIdx] = INFINITY;
+            interceptFacing[srcIdx] = 0;
+        }
+        srcIdx++;
+        dstIdx++;
+    }
+}
+
+__global__ void kernelTide(
+    int* __restrict__ rayInterceptCounts,
+    float* __restrict__ rayInterceptTs,
+    int8_t* __restrict__ rayInterceptFacing,
+    // int* __restrict__ detected,
+    // int numTriangles, 
+    int numRays
+)
+{
+    __shared__ int stride;
+    if (threadIdx.x == 0) {
+        stride = gridDim.x * blockDim.x;
+    }
+    __syncthreads();
+
+    int threadStartIdx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    for (int idx = threadStartIdx; idx < numRays; idx += stride) {
+        if (idx < numRays) {
+            int *interceptCounts = rayInterceptCounts + idx;
+            float *interceptTs = rayInterceptTs + idx * MAX_INTERSECTIONS;
+            int8_t *interceptFacing = rayInterceptFacing + idx * MAX_INTERSECTIONS;
+            tide(interceptCounts, interceptTs, interceptFacing, idx);
+        }
+    }
+}
+    
+
