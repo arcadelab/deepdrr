0129283fe40822319e165032310a6c0a752dc2c9
Benjamin Killeen <benjamindkilleen@gmail.com>
2023-09-06 16:52:59 -0400

    some install instructions

--- deepdrr/__init__.py
+++ deepdrr/__init__.py
@@ -1,7 +1,8 @@
 try:
     import cupy
 except ImportError:
-    raise ImportError("""CuPy must be installed to use DeepDRR.
+    raise ImportError(
+        """CuPy must be installed to use DeepDRR.
 Please install the version of CuPy for your CUDA Toolkit version by following the instructions here: https://cupy.dev/
 Or by installing deepdrr with the optional CuPy extra for your CUDA Toolkit version:
 pip install deepdrr[cuda102] # for CUDA 10.2
@@ -9,8 +10,11 @@ pip install deepdrr[cuda110] # for CUDA 11.0
 pip install deepdrr[cuda111] # for CUDA 11.1
 pip install deepdrr[cuda11x] # for CUDA 11.2 - 11.8
 pip install deepdrr[cuda12x] # for CUDA 12.x
-""")
-                      
+
+You can find which version of CUDA you have by running `nvcc --version` in your terminal.
+"""
+    )
+
 from . import vis, geo, projector, device, annotations, utils
 from .projector import Projector
 from .vol import Volume, Mesh
